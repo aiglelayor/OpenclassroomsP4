4f6e4e01a0536caef5440aec40180ece00212311
aiglelayor <82094142+aiglelayor@users.noreply.github.com>
2021-07-21 13:42:03 +0200

    Update UserManager.php

--- model/UserManager.php
+++ model/UserManager.php
@@ -4,7 +4,7 @@ require_once('Model/DbConnectManager.php');
 
 class UserManager extends DbConnectManager
 {
-	public function login($pseudoconnect, $passconnect)
+	public function userInfo($pseudoconnect, $passconnect)
 	{
 		$db = $this->dbConnect();
 		$requser = $db->prepare("SELECT * FROM members WHERE pseudo = ?");
@@ -12,29 +12,32 @@ class UserManager extends DbConnectManager
 		// On ajoute les champs qui correspondent à la requête dans un tableau.
 		$requser->execute(array($pseudoconnect));
 
-		$userexist = $requser->rowCount();
-		if($userexist == 1)
-		{
-			$userinfo = $requser->fetch();
-			$password_match = password_verify($passconnect, $userinfo['pass']);
+		$userinfo = $requser->fetch();
 
-			if($password_match)
-			{
-				$_SESSION['id'] = $userinfo['id'];
-				$_SESSION['isAdmin'] = $userinfo['isAdmin'];
-				$_SESSION['pseudo'] = $userinfo['pseudo'];
-				$_SESSION['email'] = $userinfo['email'];
-				//header('Location: postsManager.php');	
-			}
+		return $userinfo;
+
+		// $userexists = $requser->rowCount();
+		// if($userexists == 1)
+		// {
+		// 	$userinfo = $requser->fetch();
+		// 	$password_match = password_verify($passconnect, $userinfo['pass']);
+
+		// 	if($password_match)
+		// 	{
+		// 		$_SESSION['id'] = $userinfo['id'];
+		// 		$_SESSION['isAdmin'] = $userinfo['isAdmin'];
+		// 		$_SESSION['pseudo'] = $userinfo['pseudo'];
+		// 		$_SESSION['email'] = $userinfo['email'];
+		// 		//header('Location: postsManager.php');	
+		// 	}
 			
-		}
-		else
-		{
-			throw new Exception("Il y a une erreur dans votre pseudo ou mot de passe.");
-		}	
+		// }
+		// else
+		// {
+		// 	throw new Exception("Il y a une erreur dans votre pseudo ou mot de passe.");
+		// }	
 	}
 
-	//Ajouter au routeur
 	public function logout()
 	{
 		$_SESSION = array();
@@ -42,6 +45,46 @@ class UserManager extends DbConnectManager
 		header("Location: index.php");
 	}
 
+	public function userExists($pseudo)
+	{
+		$db = $this->dbConnect();
+		$requser = $db->prepare("SELECT * FROM members WHERE pseudo = ?");
+	
+		// On ajoute les champs qui correspondent à la requête dans un tableau.
+		$requser->execute(array($pseudo));
+
+		$userexists = $requser->rowCount();
+
+		return $userexists;
+	}
+
+	public function emailExists($email)
+	{
+		$db = $this->dbConnect();
+		$reqemail = $db->prepare("SELECT * FROM members WHERE email = ?");
+	
+		// On ajoute les champs qui correspondent à la requête dans un tableau.
+		$reqemail->execute(array($email));
+
+		$mailexists = $reqemail->rowCount();
+
+		return $mailexists;		
+	}
+
+	public function createUser($pseudo, $email, $pass)
+	{
+		$db = $this->dbConnect();
+		$pass = password_hash($_POST['pass'], PASSWORD_DEFAULT);
+		$req = $db->prepare('INSERT INTO members(pseudo, pass, email, subscription_date) VALUES(:pseudo, :pass, :email, CURDATE())');
+		$req->bindValue(':pseudo', $pseudo, PDO::PARAM_STR);
+		$req->bindValue(':email', $email, PDO::PARAM_STR);
+		$req->bindValue(':pass', $pass, PDO::PARAM_STR);
+
+		if($req->execute())
+		{
+			$_SESSION['new_account_created'] = "Votre compte a bien été créé ! Bienvenue !";
+		}
+	}
 }
 
 
